1eb77944b92552dc5e7573818a85d350f996c805
LexGrajeda <lexgrajeda@gmail.com>
2014-08-08 10:25:09 -0600

    conexion.php

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$DB_HOST = "localhost"; 
+$DB_USERNAME = "root"; 
+$DB_PASSWORD = "1682"; 
+$DB_NAME = "analisis2_practica1";  
+
+$con = mysql_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD); 
+mysql_select_db($DB_NAME, $con) or die('Error en la base de datos, Favor de intentarlo mas tarde.'); 
+
+
+?>
