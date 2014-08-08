435efe58fd67b420b8a02cd72c1d3afd6828d189
LexGrajeda <lexgrajeda@gmail.com>
2014-08-08 12:06:39 -0500

    conexion.php

--- conexion.php
+++ conexion.php
@@ -8,5 +8,8 @@ $DB_NAME = "analisis2_practica1";
 $con = mysql_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD); 
 mysql_select_db($DB_NAME, $con) or die('Error en la base de datos, Favor de intentarlo mas tarde.'); 
 
+// esto es un comentario
 
+
+//otra linea de prueba
 ?>
